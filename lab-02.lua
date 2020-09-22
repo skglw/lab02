8d66dae8d90bcb742b542605b965e7ea69fa0764
skglw <skglvv@gmail.com>
2020-09-22 14:38:24 +0300

    trigonometric added

--- lab-02.lua
+++ lab-02.lua
@@ -9,13 +9,33 @@ ui.wnd.title = 'lab-02-zhuravleva'
 ui.wnd.on_destroy = gtk.main_quit
 ui.wnd:show_all()
 
-function res()
-print('bfzsdfbz')
+function check(num)
+
 end
 
+function ui.eNum1:on_changed()
+num1 = tonumber(ui.eNum1.text)
+if (type(num1)~=number)
+then ui.lNum1.label="put the NUMBER here"
+else ui.lNum1.label="1st num: "
+end
+end
 
-ui.wnd.on_clicked = res()
+function ui.eNum2:on_changed()
+num2 = tonumber(ui.eNum2.text)
+if (type(num2)~=Number)
+then ui.lNum2.label="put the NUMBER here"
+else ui.lNum1.label="2nd num: "
+end
+end
 
+function ui.eNum3:on_changed()
+num3 = tonumber(ui.eNum3.text)
+if (type(num)~=Number)
+then ui.lNumTr.label="put the NUMBER here"
+else ui.lNumTr.label="trigonometric calculation"
+end
+end
 
 function ui.btnAdd:on_clicked(...)
 num1 = tonumber(ui.eNum1.text)
@@ -38,4 +58,13 @@ num1 = tonumber(ui.eNum1.text)
 num2 = tonumber(ui.eNum2.text)
 ui.res.label =  num1 / num2
 end
+
+
+function ui.btnSin:on_clicked(...)
+
+ui.resTr.label = ""
+end
+
+
+
 gtk.main()
