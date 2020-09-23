9535a0881ce9f9470f1669165e5c1e9faa9398c7
skglw <skglvv@gmail.com>
2020-09-23 12:39:36 +0300

    23.09

--- lab-02.lua
+++ lab-02.lua
@@ -9,13 +9,11 @@ ui.wnd.title = 'lab-02-zhuravleva'
 ui.wnd.on_destroy = gtk.main_quit
 ui.wnd:show_all()
 
-function check(num)
-
-end
+dofile('math-round.lua')
 
 function ui.eNum1:on_changed()
 num1 = tonumber(ui.eNum1.text)
-if (type(num1)~=number)
+if (type(num1)~=type(123))
 then ui.lNum1.label="put the NUMBER here"
 else ui.lNum1.label="1st num: "
 end
@@ -23,15 +21,15 @@ end
 
 function ui.eNum2:on_changed()
 num2 = tonumber(ui.eNum2.text)
-if (type(num2)~=Number)
+if (type(num2)~=type(123))
 then ui.lNum2.label="put the NUMBER here"
-else ui.lNum1.label="2nd num: "
+else ui.lNum2.label="2nd num: "
 end
 end
 
-function ui.eNum3:on_changed()
-num3 = tonumber(ui.eNum3.text)
-if (type(num)~=Number)
+function ui.eNumTr:on_changed()
+num3 = tonumber(ui.eNumTr.text)
+if (type(numTr)~=type(123))
 then ui.lNumTr.label="put the NUMBER here"
 else ui.lNumTr.label="trigonometric calculation"
 end
@@ -59,12 +57,40 @@ num2 = tonumber(ui.eNum2.text)
 ui.res.label =  num1 / num2
 end
 
-
 function ui.btnSin:on_clicked(...)
-
-ui.resTr.label = ""
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label = math.sin(num3)
 end
 
+function ui.btnCos:on_clicked(...)
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label = math.cos(num3)
+end
 
+function ui.btnTan:on_clicked(...)
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label =math.tan(num3)
+end
+
+function ui.btnAsin:on_clicked(...)
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label =math.asin(num3)
+end
+
+function ui.btnAcos:on_clicked(...)
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label =math.acos(num3)
+end
+
+function ui.btnAtan:on_clicked(...)
+num3 = tonumber(ui.eNumTr.text)
+num3 = math.rad(num3)
+ui.resTr.label =math.atan(num3)
+end
 
 gtk.main()
